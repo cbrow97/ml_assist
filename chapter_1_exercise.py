b8f2440fc776476497d4b40976a32232593eaf28
BROWN <cb049c@att.com>
2019-12-23 18:52:53 -0800

    applied fixes for remove_string_outliers function

--- chapter_1_exercise.py
+++ chapter_1_exercise.py
@@ -74,11 +74,8 @@ def remove_string_outliers(df, percent_thres=5, output=False):
     """
     remove_values_disc = []
     total_values = len(df)
+    removed_values = {}
     for column in df.columns:
-    #    try:
-    #        df.loc[0, column] + 0
-    #        
-    #    except Exception:
         if df[column].dtype not in ['int', 'long', 'float', 'complex']:
             #create list of all values present in the column    
             column_values = [value for value in df[column].value_counts().index.values]
@@ -93,19 +90,24 @@ def remove_string_outliers(df, percent_thres=5, output=False):
             #provided threshold
             values_to_remove = {value:count for (value, count) in value_counts_dict.items() if ((count / total_values) * 100) < percent_thres}                        
             
-            #A blank dictionary to add what values are to be removed and the percent of the dataset they make up. The dictionary is remade through each column loop
-            #as the remove_values_disc list is appended for each item removed and the details of that item (the colum it's in, the value, and the percent of the dataset)
-            removed_values = {}
-        
+            #Adds column+!@#+value as key and percent of total for that column/value pair to removed_values dictionary
+            #The !@# is used to strip the column and value in the block of code after this for loop
+            #I didn't add the value to the dictionary on its own incase there are other columns that have the same value
             for value, counts in values_to_remove.items():
-                df = df[df[column] != value]
-                removed_values.update({value:round(((counts / total_values) * 100), 2)})
-            
-            #Appends to remove_values_disc to later iterate over and provide the user with an output of what was removed
-            #User will need to use optional argument of output=True to utilize this functionality
-            for value, percent in removed_values.items():
-                message = f'The value of "{value}" within the column [{column}] was removed. This value only made up {percent}% of the dataset and is considered an outlier.'
-                remove_values_disc.append(message)
+                #df = df[df[column] != value]
+                removed_values.update({column + '!@#' + str(value):round(((counts / total_values) * 100), 2)})
+        
+    #Appends to remove_values_disc list to later iterate over and provide the user with an output of what was removed
+    #Uses the removed_values dictionary to iterate over and strip the column and value to their own variables
+    #These vars are then used to remove the values from the DataFrame and create the optional output message
+    for value, percent in removed_values.items():
+        column_clean = value[:value.find('!@#')]
+        value_clean =  value[value.find('!@#'):].replace('!@#', '')
+        
+        df = df[df[column_clean] != value_clean]
+
+        message = f'The value of "{value_clean}" within the column [{column_clean}] was removed. This value only made up {percent}% of the dataset and is considered an outlier.'
+        remove_values_disc.append(message)
     
     #Returns output of values that were removed
     #User will need to use optional argument of output=True to utilize this functionality
@@ -115,8 +117,9 @@ def remove_string_outliers(df, percent_thres=5, output=False):
                 print(message)
         else:
             print('No values in the passed DataFrame exceeded the percent_thres parm')
-    
+
     return df
+
 
 def fit_transform_features(df):
     try:
@@ -142,7 +145,7 @@ def fit_transform_features(df):
 
 X = remove_nulls(X, 15)
 X = remove_numerical_outliers(X)
-X = remove_string_outliers(X, percent_thres=35, output=True)
+X = remove_string_outliers(X, percent_thres=9, output=True)
 print(X)
 #X = fit_transform_features(X)
 #print(X)
--- setup_testing.py
+++ setup_testing.py
@@ -11,6 +11,37 @@ import pandas as pd
 #
 #    print(df)
 
-test_val = True
+    remove_values_disc = []
+    total_values = len(df)
+    removed_values = {}
+    for column in df.columns:
+        if df[column].dtype not in ['int', 'long', 'float', 'complex']:
+ 
+            column_values = [value for value in df[column].value_counts().index.values]
 
-print(test_val - 5)
+            value_counts = [count for count in df[column].value_counts().values]
+            
+            value_counts_dict = dict(zip(column_values, value_counts))
+
+            values_to_remove = {value:count for (value, count) in value_counts_dict.items() if ((count / total_values) * 100) < percent_thres}                        
+
+            for value, counts in values_to_remove.items():
+                removed_values.update({column + '!@#' + str(value):round(((counts / total_values) * 100), 2)})
+
+    for value, percent in removed_values.items():
+        column_clean = value[:value.find('!@#')]
+        value_clean =  value[value.find('!@#'):].replace('!@#', '')
+        
+        df = df[df[column_clean] != value_clean]
+
+        message = f'The value of "{value_clean}" within the column [{column_clean}] was removed. This value only made up {percent}% of the dataset and is considered an outlier.'
+        remove_values_disc.append(message)
+
+    if output:
+        if remove_values_disc:
+            for message in remove_values_disc:
+                print(message)
+        else:
+            print('No values in the passed DataFrame exceeded the percent_thres parm')
+
+    return df
